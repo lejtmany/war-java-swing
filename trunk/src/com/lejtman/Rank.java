6ac42dad5cf3621a31fbf2bdb3639931c5b3abf6
lejtmany <lejtmany@e1b2d6cf-83e4-12e9-3a5e-0089f2a20cea>
2015-02-05 02:26:35 +0000

    added shuffle method NOT using collections.shuffle for HW

--- trunk/src/com/lejtman/Rank.java
+++ trunk/src/com/lejtman/Rank.java
@@ -7,12 +7,12 @@
 package com.lejtman;
 
 
-public enum Value {
-    TWO(2),THREE(3),FOUR(4),FIVE(5),SIX(6),SEVEN(7),EIGHT(8),NINE(9),TEN(10),JACK(11),QUEEN(12),KING(13),ACE(14);
+public enum Rank {
+    ACE(1),TWO(2),THREE(3),FOUR(4),FIVE(5),SIX(6),SEVEN(7),EIGHT(8),NINE(9),TEN(10),JACK(11),QUEEN(12),KING(13);
     
     private int value;
     
-     Value(int value){
+     Rank(int value){
         this.value = value;
     }
      
